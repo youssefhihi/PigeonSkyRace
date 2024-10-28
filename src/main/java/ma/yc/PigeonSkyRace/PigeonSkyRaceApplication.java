dd85b38031a96f0663a7a1b342357521a51fcd90
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-10-28 17:03:01 +0100

    Added : project set-up PIG-19

---
+++ src/main/java/ma/yc/PigeonSkyRace/PigeonSkyRaceApplication.java
@@ -0,0 +1,13 @@
+package ma.yc.PigeonSkyRace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigeonSkyRaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PigeonSkyRaceApplication.class, args);
+	}
+
+}
