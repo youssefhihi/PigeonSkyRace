055eacb7c3b787e7b06546a8cc7ba83861aa4597
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-11-11 05:59:36 +0100

    Switch from @OpenAPIDefinition to custom OpenAPI configuration for API documentation setup PIG-72

--- src/main/java/ma/yc/PigeonSkyRace/PigeonSkyRaceApplication.java
+++ src/main/java/ma/yc/PigeonSkyRace/PigeonSkyRaceApplication.java
@@ -1,27 +1,19 @@
 package ma.yc.PigeonSkyRace;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@OpenAPIDefinition(
-		info = @Info(
-				title = "Pigeon Sky Race API",
-				version = "1.0",
-				description = "API documentation for managing pigeon racing competitions with MongoDB"
-		)
-)
+
 @SpringBootApplication(scanBasePackages = "ma.yc.PigeonSkyRace")
 @EnableMongoRepositories
 @EnableScheduling
 public class PigeonSkyRaceApplication {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(PigeonSkyRaceApplication.class, args);
+    public static void main ( String[] args ) {
+        ConfigurableApplicationContext context = SpringApplication.run(PigeonSkyRaceApplication.class, args);
 
-	}
+    }
 }
---
+++ src/main/java/ma/yc/PigeonSkyRace/config/OpenApiConfig.java
@@ -0,0 +1,40 @@
+package ma.yc.PigeonSkyRace.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@Slf4j
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${contact.email}") String email,
+                                 @Value("${local-server.url}") String localServerUrl,
+                                 @Value("${prod-server.url}") String prodServerUrl,
+                                 @Value("${project.version}") String appVersion,
+                                 @Value("${project.title}") String title,
+                                 @Value("${project.description}") String description) {
+        log.info("email: {}", email);
+        log.info("localServerUrl: {}", localServerUrl);
+
+        return new OpenAPI().info(new Info()
+                .title(title)
+                .version(appVersion)
+                .description(description)
+                .contact(new Contact()
+                        .name("Codex Pigeon Sky Race API")
+                        .email(email))
+        ).servers(Arrays.asList(
+                new Server().description("Local Docker Server").url(localServerUrl),
+                new Server().description("Production Server (inshallah soon when i deploy on aws)").url(prodServerUrl)
+        ));
+    }
+}
