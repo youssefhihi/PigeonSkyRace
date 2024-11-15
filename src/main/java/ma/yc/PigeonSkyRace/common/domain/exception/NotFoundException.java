fdf2603a75639d35da35ca50f06985e298f0151b
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-11-15 17:16:22 +0100

    Added Loft service tests PIG-13

--- src/main/java/ma/yc/PigeonSkyRace/common/domain/exception/NotFoundException.java
+++ src/main/java/ma/yc/PigeonSkyRace/common/domain/exception/NotFoundException.java
@@ -4,13 +4,7 @@ import ma.yc.PigeonSkyRace.user.domain.model.valueobject.UserId;
 
 public class NotFoundException extends RuntimeException {
     public <T> NotFoundException(String entity, T id) {
-        super(entity + " with id " + extractIdValue(id) + " not found");
+        super(entity + " with id " + id + " not found");
     }
 
-    private static <T> String extractIdValue(T id) {
-        if (id instanceof UserId userId) {
-            return userId.toHexString();
-        }
-        return id.toString();
-    }
 }
