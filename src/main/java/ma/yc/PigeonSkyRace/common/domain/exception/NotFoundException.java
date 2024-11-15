85835f29c02ffb7be5a1b713cadb4bc513b45d3e
bouanani-soufiane <soufian.bouanani2013@gmail.com>
2024-11-15 16:43:25 +0100

    Added User service tests PIG-2

--- src/main/java/ma/yc/PigeonSkyRace/common/domain/exception/NotFoundException.java
+++ src/main/java/ma/yc/PigeonSkyRace/common/domain/exception/NotFoundException.java
@@ -1,12 +1,16 @@
 package ma.yc.PigeonSkyRace.common.domain.exception;
 
+import ma.yc.PigeonSkyRace.user.domain.model.valueobject.UserId;
 
-import ma.yc.PigeonSkyRace.competition.domain.ValueObject.SeasonId;
-
-public class NotFoundException  extends RuntimeException {
+public class NotFoundException extends RuntimeException {
     public <T> NotFoundException(String entity, T id) {
-        super(entity + " with id " + id + " not found");
+        super(entity + " with id " + extractIdValue(id) + " not found");
     }
 
-
+    private static <T> String extractIdValue(T id) {
+        if (id instanceof UserId userId) {
+            return userId.toHexString();
+        }
+        return id.toString();
+    }
 }
